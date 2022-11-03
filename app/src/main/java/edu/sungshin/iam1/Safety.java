ce4ca1fa5a4abd5dfcee3e8936a0b302aa1261a8
sunkyoung19 <93823419+sunkyoung19@users.noreply.github.com>
2022-11-03 13:42:37 +0900

    Safety.java change

--- app/src/main/java/edu/sungshin/iam1/Safety.java
+++ app/src/main/java/edu/sungshin/iam1/Safety.java
@@ -2,13 +2,39 @@ package edu.sungshin.iam1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Safety extends AppCompatActivity {
+
+    Button call_112, call_119;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_safety);
+
+        Button call_112 = (Button) findViewById(R.id.button1);
+        Button call_119 = (Button) findViewById(R.id.button2);
+
+        call_112.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Uri uri = Uri.parse("tel:/112");
+                Intent intent = new Intent(Intent.ACTION_DIAL, uri);
+                startActivity(intent);
+            }
+        });
+        call_119.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Uri uri = Uri.parse("tel:/119");
+                Intent intent = new Intent(Intent.ACTION_DIAL, uri);
+                startActivity(intent);
+            }
+        });
     }
 }
