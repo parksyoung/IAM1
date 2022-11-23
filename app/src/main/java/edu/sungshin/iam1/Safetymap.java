e0f3cda061ebf5efdcc5ccae37a6af85e8a211b7
yugyeongheo <20211416@sungshin.ac.kr>
2022-11-24 01:00:12 +0900

    .

--- app/src/main/java/edu/sungshin/iam1/Safetymap.java
+++ app/src/main/java/edu/sungshin/iam1/Safetymap.java
@@ -15,9 +15,10 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class Safetymap extends AppCompatActivity {
+public class Safetymap extends AppCompatActivity
+        implements OnMapReadyCallback {
 
-
+    private GoogleMap mMap;
     Button call_112, call_119;
 
     @Override
@@ -44,5 +45,29 @@ public class Safetymap extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
     }
+
+    @Override
+    public void onMapReady(final GoogleMap googleMap) {
+
+        mMap = googleMap;
+
+        LatLng SEOUL = new LatLng(37.56, 126.97);
+
+        MarkerOptions markerOptions = new MarkerOptions();         // 마커 생성
+        markerOptions.position(SEOUL);
+        markerOptions.title("서울");                         // 마커 제목
+        markerOptions.snippet("한국의 수도");         // 마커 설명
+        mMap.addMarker(markerOptions);
+
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(SEOUL));                 // 초기 위치
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(15));                         // 줌의 정도
+        googleMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);                           // 지도 유형 설정
+
+    }
+
 }
