661c7e71e017889c4c0b5a8dfb3d00f313218ce5
siyeon yoo <syne77777@gmail.com>
2022-11-24 00:48:06 +0900

    깃 확인용

--- app/src/main/java/edu/sungshin/iam1/Checklist.java
+++ app/src/main/java/edu/sungshin/iam1/Checklist.java
@@ -1,49 +1,91 @@
 package edu.sungshin.iam1;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
+import androidx.fragment.app.Fragment;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.CalendarView;
 import android.widget.EditText;
-
-import java.util.ArrayList;
+import android.widget.Toast;
 
 public class Checklist extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
 
-    Button insert;
-    EditText todoEdit;
-    RecyclerView recyclerView;
-    private ArrayList<Todo> todoArrayList;
-    private TodoAdapter todoAdapter;
+    Fragment mainFragment;
+    EditText inputToDo;
+    Context context;
+
+    public static NoteDatabase noteDatabase = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_checklist);
-        insert = (Button) findViewById(R.id.insert);
-        todoEdit = (EditText) findViewById(R.id.todoList);
-        recyclerView = (RecyclerView) findViewById(R.id.recycler);
 
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        mainFragment = new TodoFragment();
 
-        todoArrayList = new ArrayList<>();
-        todoAdapter = new TodoAdapter(todoArrayList);
-        recyclerView.setAdapter(todoAdapter);
+        //getSupportFragmentManager 을 이용하여 이전에 만들었던 **FrameLayout**에 `fragment_main.xml`이 추가
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, mainFragment).commit();
 
-        insert.setOnClickListener(new View.OnClickListener() {
+        Button saveButton = findViewById(R.id.saveButton);
+        saveButton.setOnClickListener(new View.OnClickListener(){
             @Override
-            public void onClick(View view) {
-                Todo newTodo = new Todo(todoEdit.getText().toString());
-                todoArrayList.add(newTodo);
-                todoAdapter.notifyDataSetChanged();
-                todoEdit.setText(null);
+            public void onClick(View view){
+
+                saveToDo();
+
+                Toast.makeText(getApplicationContext(),"추가되었습니다.",Toast.LENGTH_SHORT).show();
+
             }
         });
-
+        openDatabase();
     }
+    private void saveToDo(){
+        inputToDo = findViewById(R.id.inputToDo);
+
+        //EditText에 적힌 글을 가져오기
+        String todo = inputToDo.getText().toString();
+
+        //테이블에 값을 추가하는 sql구문 insert...
+        String sqlSave = "insert into " + NoteDatabase.TABLE_NOTE + " (TODO) values (" +
+                "'" + todo + "')";
+
+        //sql문 실행
+        NoteDatabase database = NoteDatabase.getInstance(context);
+        database.execSQL(sqlSave);
+
+        //저장과 동시에 EditText 안의 글 초기화
+        inputToDo.setText("");
+    }
+
+
+    public void openDatabase() {
+        // open database
+        if (noteDatabase != null) {
+            noteDatabase.close();
+            noteDatabase = null;
+        }
+
+        noteDatabase = NoteDatabase.getInstance(this);
+        boolean isOpen = noteDatabase.open();
+        if (isOpen) {
+            Log.d(TAG, "Note database is open.");
+        } else {
+            Log.d(TAG, "Note database is not open.");
+        }
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        if (noteDatabase != null) {
+            noteDatabase.close();
+            noteDatabase = null;
+        }
+    }
+
 
 }
