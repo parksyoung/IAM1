08a1133014e8c6354e917e8c6766d4550e87fc46
parksyoung <qkrtjdudsy@naver.com>
2022-11-26 12:38:46 +0900

    뒤로가기 버튼 두 번 클릭시 앱 종료& 메인 메뉴 폰트 수정

--- app/src/main/java/edu/sungshin/iam1/MainActivity.java
+++ app/src/main/java/edu/sungshin/iam1/MainActivity.java
@@ -1,12 +1,12 @@
 package edu.sungshin.iam1;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.auth.FirebaseAuth;
 
@@ -14,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
 
     FirebaseAuth mAuth;
     Button community, checklist, safetymap, mypage, logout, delAccount;
+    private long backKeyPressedTime = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -83,7 +84,25 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(getApplicationContext(), "회원탈퇴 완료되셨습니다.",Toast.LENGTH_SHORT).show();
             }
         });
+    }
 
+    @Override
+    public void onBackPressed() {
+        //super.onBackPressed();
+        // 기존 뒤로 가기 버튼의 기능을 막기 위해 주석 처리 또는 삭제
 
+        // 마지막으로 뒤로 가기 버튼을 눌렀던 시간에 2.5초를 더해 현재 시간과 비교 후
+        // 마지막으로 뒤로 가기 버튼을 눌렀던 시간이 2.5초가 지났으면 Toast 출력
+        // 2500 milliseconds = 2.5 seconds
+        if (System.currentTimeMillis() > backKeyPressedTime + 2500) {
+            backKeyPressedTime = System.currentTimeMillis();
+            Toast.makeText(this, "뒤로 가기 버튼을 한 번 더 누르면 종료됩니다.", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        // 마지막으로 뒤로 가기 버튼을 눌렀던 시간에 2.5초를 더해 현재 시간과 비교 후
+        // 마지막으로 뒤로 가기 버튼을 눌렀던 시간이 2.5초가 지나지 않았으면 종료
+        if (System.currentTimeMillis() <= backKeyPressedTime + 2500) {
+            finish();
+        }
     }
 }
