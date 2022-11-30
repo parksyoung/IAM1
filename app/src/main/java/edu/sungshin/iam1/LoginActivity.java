a923d8fa9b64de5d8525700c8e3bcd1a1fcf6826
parksyoung <qkrtjdudsy@naver.com>
2022-12-01 00:34:11 +0900

    로그인 정보 기억하기 추가

--- app/src/main/java/edu/sungshin/iam1/LoginActivity.java
+++ app/src/main/java/edu/sungshin/iam1/LoginActivity.java
@@ -3,10 +3,12 @@ package edu.sungshin.iam1;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.content.Intent;
 import android.widget.Button;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -22,17 +24,33 @@ public class LoginActivity extends AppCompatActivity {
     private FirebaseAuth mAuth; //파이어베이스 인증 처리
     private DatabaseReference mDB; // 실시간 데이터베이스
     private EditText id, pw;
+    private CheckBox checkBox;
+    private boolean saveLoginData;
+    private String Id, Pwd;
+    private SharedPreferences appData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        // 설정값 불러오기
+        appData = getSharedPreferences("appData", MODE_PRIVATE);
+        load();
+
+        id = (EditText) findViewById(R.id.edit_id);
+        pw = (EditText) findViewById(R.id.edit_pwd);
+        checkBox = (CheckBox) findViewById(R.id.checkBox);
+
+        // 이전에 로그인 정보를 저장시킨 기록이 있다면
+        if (saveLoginData) {
+            id.setText(Id);
+            pw.setText(Pwd);
+            checkBox.setChecked(saveLoginData);
+        }
+
         mAuth = FirebaseAuth.getInstance();
         mDB = FirebaseDatabase.getInstance().getReference("IAM");
-
-        id = findViewById(R.id.edit_id);
-        pw = findViewById(R.id.edit_pwd);
 
         Button btn_login = (Button) findViewById(R.id.btn_login);
         Button btn_signin = (Button) findViewById(R.id.btn_signin);
@@ -49,6 +67,7 @@ public class LoginActivity extends AppCompatActivity {
                         @Override
                         public void onComplete(@NonNull Task<AuthResult> task) {
                             if(task.isSuccessful()){
+                                save();
                                 //로그인 성공
                                 Intent intent = new Intent(LoginActivity.this,MainActivity.class);
                                 startActivity(intent);
@@ -75,6 +94,27 @@ public class LoginActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+    private void save() {
+        // SharedPreferences 객체만으론 저장 불가능 Editor 사용
+        SharedPreferences.Editor editor = appData.edit();
 
+        // 에디터객체.put타입( 저장시킬 이름, 저장시킬 값 )
+        // 저장시킬 이름이 이미 존재하면 덮어씌움
+        editor.putBoolean("SAVE_LOGIN_DATA", checkBox.isChecked());
+        editor.putString("ID", id.getText().toString().trim());
+        editor.putString("PWD", pw.getText().toString().trim());
+
+        // apply, commit 을 안하면 변경된 내용이 저장되지 않음
+        editor.apply();
+    }
+
+    // 설정값을 불러오는 함수
+    private void load() {
+        // SharedPreferences 객체.get타입( 저장된 이름, 기본값 )
+        // 저장된 이름이 존재하지 않을 시 기본값
+        saveLoginData = appData.getBoolean("SAVE_LOGIN_DATA", false);
+        Id = appData.getString("ID", "");
+        Pwd = appData.getString("PWD", "");
     }
 }
