4a43037d8f28a137e36fc56def9b5385e126c341
parksyoung <qkrtjdudsy@naver.com>
2022-12-01 15:18:46 +0900

    구글맵 수정2

--- app/src/main/java/edu/sungshin/HomeAlone/Safetymap.java
+++ app/src/main/java/edu/sungshin/HomeAlone/Safetymap.java
@@ -82,7 +82,7 @@ public class Safetymap extends AppCompatActivity
 
         mMap.moveCamera(CameraUpdateFactory.newLatLng(SEOUL));                 // 초기 위치
         mMap.animateCamera(CameraUpdateFactory.zoomTo(15));                         // 줌의 정도
-        googleMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);                           // 지도 유형 설정
+        googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);                           // 지도 유형 설정
 
     }
 
